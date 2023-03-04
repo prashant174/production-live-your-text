b5b1c83ac9c4800783996138d0e7b0ad41f382c2
Prashant Kumar Yadav <yprashant174@gmail.com>
2023-03-05 01:40:39 +0530

    jmd

--- server.js
+++ server.js
@@ -64,10 +64,10 @@ io.on('connection',(socket)=>{
 
 })
 
-app.use(express.static(path.join(__dirname, './build')))
+app.use(express.static(path.join(__dirname,'build')))
 
 app.get("*", function(req,res){
-    res.sendFile(path.join(__dirname, "./build/index.html"))
+    res.sendFile(path.join(__dirname,"build/index.html"))
 })
 
 const port=process.env.PORT||8080
